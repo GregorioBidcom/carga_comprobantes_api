db38fe3b42aee8c57c37e9d6143bbc2171b4ad40
Leandro Martinez <leandro.martinez@thelucky.app>
2024-06-26 09:43:50 -0300

    trying commit

--- src/comprobantes/comprobantes.controller.ts
+++ src/comprobantes/comprobantes.controller.ts
@@ -4,21 +4,22 @@ import { ComprobanteService } from './comprobantes.service';
 
 @Controller('comprobantes')
 export class ComprobantesController {
-  constructor(private compService: ComprobanteService) {}
+    constructor(private compService: ComprobanteService) {
+    }
 
-  @Get(':idComprobante') readComprobante(
-    @Param('idComprobante') idComprobante: number,
-  ): Promise<Comprobante[]> {
-    return this.compService.getOneComprobante(idComprobante);
-  }
+    @Get(':idComprobante') readComprobante(
+        @Param('idComprobante') idComprobante: number,
+    ): Promise<Comprobante[]> {
+        return this.compService.getOneComprobante(idComprobante);
+    }
 
-  @Post('nuevoComprobante')
-  createComprobante(@Body() newComprobante: Comprobante) {
-    return this.compService.createComprobante(newComprobante);
-  }
+    @Post('nuevoComprobante')
+    createComprobante(@Body() newComprobante: Comprobante) {
+        return this.compService.createComprobante(newComprobante);
+    }
 
-  @Post('editarComprobante')
-  editComprobante(@Body() editComprobante: Comprobante) {
-    return this.compService.editarComprobante(editComprobante);
-  }
+    @Post('editarComprobante')
+    editComprobante(@Body() editComprobante: Comprobante) {
+        return this.compService.editarComprobante(editComprobante);
+    }
 }
